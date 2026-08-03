312004a794398c0baab138a95fd6579e4a8023c0
31207 <1692038362@qq.com>
2026-08-03 13:13:53 +0800

    更新了一下nbrs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-use nonebot_rs;
-use mybot::*;
 use builtin_plugins::matcher::prelude::*;
+use mybot::*;
+use nonebot_rs;
 fn main() {
     let mut nb = nonebot_rs::Nonebot::new();
     let mut matchers = Matchers::new_empty();
     matchers.add_message_matchers(vec![
-        meme_parser::meme_parser(),
+        meme_parser::meme_parser().disable_matched_log(),
         helper::helper(),
         hzys::hzys(),
         otto_quotation::otto_quotation(),
-        ]);
+    ]);
     nb.add_plugin(matchers);
-    nb.add_plugin(builtin_plugins::logger::Logger);
+    nb.add_plugin(builtin_plugins::logger::Logger::new());
     nb.run()
 }
